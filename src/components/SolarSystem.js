e6b5e3152605619619243a6d46a00e3e3e9004a5
Katia Nienow <nienowkatia@gmail.com>
2023-03-01 17:33:57 -0300

    requisitos 5 e 6

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,14 +1,36 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {
+          planets
+            .map((planet) => (
+              <li key={ planet.name }>
+                <PlanetCard planetName={ planet.name } planetImage={ planet.image } />
+              </li>
+            ))
+        }
       </div>
     );
   }
 }
+
+SolarSystem.defaultProps = {
+  planets: [],
+};
+
+// SolarSystem.propTypes = {
+//   planets: PropTypes.arrayOf(PropTypes.shape({
+//     name: PropTypes.string,
+//     image: PropTypes.string,
+//   })),
+// };
 
 export default SolarSystem;
